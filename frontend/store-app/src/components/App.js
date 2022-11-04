944ad2e22b2d2faa639b76cecea4f8ca690fa588
Benjamin Klarer <benjaminklarer@gmail.com>
2022-11-04 14:28:20 -0700

    Clean up code not needed from create react app

---
+++ frontend/store-app/src/components/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect} from 'react';
+import { Route, Switch } from 'react-router-dom';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
