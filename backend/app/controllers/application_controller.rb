913c8d7874bb643c7e969b11e9653adbb9495014
Benjamin Klarer <benjaminklarer@gmail.com>
2022-11-04 23:17:53 -0700

    Finish post routes in application_controller.rb

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -36,6 +36,16 @@ class ApplicationController < Sinatra::Base
     product.to_json(include: :reviews)
   end
 
+  post "/products" do
+    product = Product.create(
+    name: params[:name],
+    price: params[:price],
+    img_url: params[:img_url],
+    store_id: params[:store_id]
+    )
+    product.to_json
+  end
+
   delete "/products/:id" do
     product = Product.find(params[:id])
     product.destroy
@@ -49,6 +59,17 @@ class ApplicationController < Sinatra::Base
 
   get "reviews/:id" do
     review = Review.find(params[:id])
+    review.to_json
+  end
+
+  post "/reviews" do
+    review = Review.create(
+    reviewer_first_name: params[:reviewer_first_name],
+    date: params[:date],
+    stars: params[:stars],
+    review_text: params[:review_text],
+    product_id: params[:product_id]
+    )
     review.to_json
   end
 
