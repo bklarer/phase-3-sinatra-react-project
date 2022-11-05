a9fb6fac0ba44afab946464a2317f27515cbbbbb
Benjamin Klarer <benjaminklarer@gmail.com>
2022-11-04 20:58:36 -0700

    create get requests for all models and post request for store

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,61 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/stores" do
+    stores = Store.all
+    stores.to_json
+  end
+
+  get "/stores/:id" do
+    store = Store.find(params[:id])
+    store.to_json(include: :products)
+  end
+
+  post "/stores" do
+    store = Store.create(
+      name: params[:name],
+      zipcode: params[:zipcode].
+      img_url: params[:img_url]
+    )
+    store.to_json
+  end
+
+  delete "/stores/:id" do
+    store = Store.find(params[:id])
+    store.destroy
+    store.to_json
+  end 
+
+  get "/products" do
+    products = Product.all
+    products.to_json
+  end
+
+  get "/products/:id" do
+    product = Product.find(params[:id])
+    product.to_json(include: :reviews)
+  end
+
+  delete "/products/:id" do
+    product = Product.find(params[:id])
+    product.destroy
+    product.to_json
+  end
+
+  get "/reviews" do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  get "reviews/:id" do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
   end
 
 end
