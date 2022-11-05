674d438d72fefe2cb7d232e2cc8e1d688cd6a07f
Benjamin Klarer <benjaminklarer@gmail.com>
2022-11-05 00:09:40 -0700

    Add seeder gem code for seeding

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,5 +1,30 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+    5.times do
+        Store.create(
+            name: Faker::Address.community,
+            zipcode: Faker::Address.zip_code,
+            img_url: Faker::Avatar.image
+        )
+    end
+
+    25.times do
+        Product.create(
+            name: Faker::Game.title,
+            price: Faker::Commerce.price,
+            img_url: Faker::Avatar.image,
+            store_id: Faker::Number.within(range: 1..5)
+        )
+    end
+
+    50.times do
+        Review.create(
+            reviewer_first_name: Faker::Name.first_name,
+            date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+            stars: Faker::Number.within(range: 1..5)
+            review_text: Faker::Quote.famous_last_words
+            product_id: Faker::Number.within(range: 1..25)
+        )
+    end
 
 puts "✅ Done seeding!"
