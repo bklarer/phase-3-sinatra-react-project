d42b81360df9f4713384a8208af9afe4f43b5cbf
Benjamin Klarer <benjaminklarer@gmail.com>
2022-11-05 14:46:29 -0700

    Add home page

--- frontend/store-app/src/components/App.js
+++ frontend/store-app/src/components/App.js
@@ -1,13 +1,20 @@
 import { useState, useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '../App.css';
+import Home from './Home';
 import StoreContainer from './StoreContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
-      <StoreContainer/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/stores">
+          <StoreContainer/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ frontend/store-app/src/components/Home.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Home () {
+
+
+
+
+
+
+
+    return(
+        <div>
+            <h2>Welcome to the Store App!</h2>
+            <p>Please click the link below to see the available stores</p>
+            <Link to="/stores">See Stores</Link>
+        </div>
+
+
+    )
+
+}
+
+export default Home;
